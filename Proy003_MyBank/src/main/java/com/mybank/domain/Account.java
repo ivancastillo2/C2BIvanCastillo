3dcbea49131d7564df2400056db74410af8a867a
ivancastillo2 <icascan1@gmail.com>
2022-10-05 10:42:50 +0200

    Renombramiento de proyecto

--- Proy003_MyBank/src/main/java/com/mybank/domain/Account.java
+++ Proy003_MyBank/src/main/java/com/mybank/domain/Account.java
@@ -1,5 +1,13 @@
 package com.mybank.domain;
 
+/**
+ * Clase que representa una cuenta bancaria.
+ * De una cuenta bancaria se puede retirar dinero
+ * y depositar dinero.
+ * También se puede comprobar el saldo actual de la cuenta.
+ * @author Iván
+ *
+ */
 public class Account {
 	private double balance;
 	
@@ -7,15 +15,30 @@ public class Account {
 		this.balance = balance;
 	}
 	
+	
+	/**
+	 * Método que devuelve el saldo actual de la cuenta bancaria.
+	 * @return Saldo actual de la cuena bancaria.
+	 */
 	public double getBalance() {
 		return balance;
 	}
 	
+	/**
+	 * Método que sirve para depositar dinero en a cuenta bancaria.
+	 * @param amt Cantidad de dinero que quieres depositar en la cuenta bancaria.
+	 * @return True si a operación se ha realizado con exito.
+	 */
 	public boolean deposit(double amt) {
 		balance += amt;
 		return true;
 	}
 	
+	/**
+	 * Método que sirve para sacar dinero de la cuenta bancaria.
+	 * @param amt Cantidad de dinero que quieres depositar en la cuenta bancaria.
+	 * @return True si la operación se ha realizado con exite, False si ha habido algun error durante la operación.
+	 */
 	public boolean withdraw(double amt) {
 		if(amt > balance) {
 			return false;
--- Proy003_MyBank/src/main/java/com/mybank/domain/Customer.java
+++ Proy003_MyBank/src/main/java/com/mybank/domain/Customer.java
@@ -1,5 +1,10 @@
 package com.mybank.domain;
 
+/**
+ * Clase que representa un cliente que puede tener una cuena bancaria.
+ * @author Iván
+ *
+ */
 public class Customer {
 	private String firstName;
 	private String lastName;
@@ -10,18 +15,34 @@ public class Customer {
 		this.lastName = lastName;
 	}
 	
+	/**
+	 * Método que devuelve el nombre del cliente.
+	 * @return Nombre del cliente.
+	 */
 	public String getFirstName() {
 		return firstName;
 	}
 	
+	/**
+	 * Método que devuelve el apellido de cliente.
+	 * @return Apelido del cliente.
+	 */
 	public String getLastName() {
 		return lastName;
 	}
 	
+	/**
+	 * Método que devuelve la cuenta bancaria del cliente.
+	 * @return Cuenta bancaria del cliente.
+	 */
 	public Account getAccount() {
 		return account;
 	}
 	
+	/**
+	 * Método que le asigna una cuenta bancaria a un cliente.
+	 * @param account Cuenta bancaria que le quieres asignar al cliente.
+	 */
 	public void setAccount(Account account) {
 		this.account = account;
 	}
